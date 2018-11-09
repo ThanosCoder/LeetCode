32365c13925b080ca7425adbe706cdba5e3c336c
ThanosCoder <ThanosCoder@gmail.com>
2018-11-10 00:07:14 +0800

    add package

---
+++ src/main/java/io/sage/leetcode/ex0001/TwoSum.java
@@ -0,0 +1,9 @@
+package io.sage.leetcode.ex0001;
+
+/**
+ * @author YangSiji
+ * @email yangsiji@gf.com.cn
+ * @create 2018-11-10 12:05 AM
+ **/
+public class TwoSum {
+}
---
+++ src/main/java/io/sage/leetcode/ex0002/AddTwoNumbers.java
@@ -0,0 +1,9 @@
+package io.sage.leetcode.ex0002;
+
+/**
+ * @author YangSiji
+ * @email yangsiji@gf.com.cn
+ * @create 2018-11-10 12:06 AM
+ **/
+public class AddTwoNumbers {
+}
